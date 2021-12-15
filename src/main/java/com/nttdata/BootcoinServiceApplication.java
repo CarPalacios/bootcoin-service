8347ddd8e9f1fd85337498e11aa87a0e50b3a659
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-12-15 15:50:14 -0500

    Segundo Commit

--- src/main/java/com/nttdata/BootcoinServiceApplication.java
+++ src/main/java/com/nttdata/BootcoinServiceApplication.java
@@ -2,10 +2,11 @@ package com.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BootcoinServiceApplication {
 
 	public static void main(String[] args) {
