ce0ca6bcaa4d4e683edc5156432a9d20710241a8
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-12-15 15:47:52 -0500

    Segundo Commit

--- src/main/java/com/nttdata/service/impl/RateServiceImpl.java
+++ src/main/java/com/nttdata/service/impl/RateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.nttdata.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.nttdata.model.Rate;
 import com.nttdata.repository.RateRepository;
@@ -9,6 +10,7 @@ import com.nttdata.service.RateService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class RateServiceImpl implements RateService {
   
   @Autowired
